600a237d07619540443aaf01c711d03794996df4
H1ddenGames <skarim3000@gmail.com>
2019-01-20 00:42:16 -0800

    Added hit animation, updated destructible script
    The destructible script now waits until the block has been hit a specified number of times before breaking.
    When the hits left variable is greater than 0, an animation will play that gives the impression of the block being hit.
    There is an option called isExplosive that decides if only one random block will receive the explosion force or if all the blocks do.
    
    Fixed the rotation snapping back to the rotation of the prefab. This was fixed by creating a parent empty GameObject and putting the prefab as the child of the empty GameObject. Put the animator on the parent and everything works properly.
    
    Signed-off-by: H1ddenGames <skarim3000@gmail.com>

--- Assets/Scripts/Other Scripts/Destructible.cs
+++ Assets/Scripts/Other Scripts/Destructible.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Animations;
 using System.Linq;
 
 namespace H1ddenGames
 {
     namespace DestructibleEnvironment
     {
-        public class Destructible : MonoBehaviour, IPointerClickHandler 
+        public class Destructible : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         {
             [Header("Force's Power"), Tooltip("How much force should be given to the object's pieces?"), SerializeField] private float _minExplosionForce = 1000f;
             [SerializeField] private float _maxExplosionForce = 1800f;
+            [SerializeField] private int _hitsToBreak = 1;
+            [SerializeField] private bool _isExplosive = false;
 
             [Tooltip("How big is the explosion's radius?"), SerializeField] private float _minExplosionRadius = 3f;
             [SerializeField] private float _maxExplosionRadius = 4.5f;
@@ -25,15 +28,17 @@ namespace H1ddenGames
 
             [Space(10), SerializeField] private Material _replacementMaterial;
 
-            [SerializeField] private BoxCollider _boxCollider;
-            [SerializeField, Space(10)] private List<Rigidbody> _rigidbodies = new List<Rigidbody>();
-            [SerializeField] private List<MeshRenderer> _renderers = new List<MeshRenderer>();
-            [SerializeField] private List<float> _distances = new List<float>();
+            private BoxCollider _boxCollider;
+            private List<Rigidbody> _rigidbodies = new List<Rigidbody>();
+            private List<MeshRenderer> _renderers = new List<MeshRenderer>();
+            private List<float> _distances = new List<float>();
 
             private Vector3 _pos;
             private RaycastHit[] _hits;
-            public List<GameObject> _gameObjectsToCleanUp = new List<GameObject>();
+            private List<GameObject> _gameObjectsToCleanUp = new List<GameObject>();
             private Coroutine co;
+
+            public Animator _animator; 
 
             #region Getters and Setters
             public List<Rigidbody> Rigidbodies { get => _rigidbodies; set => _rigidbodies = value; }
@@ -42,6 +47,7 @@ namespace H1ddenGames
             public BoxCollider BoxCollider { get => _boxCollider; set => _boxCollider = value; }
             public Material ReplacementMaterial { get => _replacementMaterial; set => _replacementMaterial = value; }
 
+            public int HitsToBreak { get => _hitsToBreak; set => _hitsToBreak = value; }
             public float MinExplosionForce { get => _minExplosionForce; set => _minExplosionForce = value; }
             public float MaxExplosionForce { get => _maxExplosionForce; set => _maxExplosionForce = value; }
             public float MinExplosionRadius { get => _minExplosionRadius; set => _minExplosionRadius = value; }
@@ -50,21 +56,23 @@ namespace H1ddenGames
             public float MinExplosionRange { get => _minExplosionRange; set => _minExplosionRange = value; }
             public float MaxExplosionRange { get => _maxExplosionRange; set => _maxExplosionRange = value; }
             public float CleanUpTime { get => _cleanUpTime; set => _cleanUpTime = value; }
+            public bool IsExplosive { get => _isExplosive; set => _isExplosive = value; }
             #endregion
 
             private void Awake()
             {
                 if(BoxCollider == null) { BoxCollider = gameObject.GetComponent<BoxCollider>(); }
-                if(Rigidbodies == null) { Rigidbodies = gameObject.GetComponentsInChildren<Rigidbody>().ToList(); }
-                if(Renderers == null) { Renderers = gameObject.GetComponentsInChildren<MeshRenderer>().ToList(); }
-                if(Distances == null) { UpdateDistances(); }
+                if(Rigidbodies == null || Rigidbodies.Count == 0) { Rigidbodies = gameObject.GetComponentsInChildren<Rigidbody>().ToList(); }
+                if(Renderers == null || Renderers.Count == 0) { Renderers = gameObject.GetComponentsInChildren<MeshRenderer>().ToList(); }
+                if(Distances == null || Distances.Count == 0) { UpdateDistances(); }
 
                 ToggleRigidbodies(true);
+                _animator = transform.GetComponentInParent<Animator>();
             }
 
             public void LateUpdate()
             {
-                UpdateClickableArea();
+                ReplaceMaterials();
             }
 
             /// <summary>
@@ -105,21 +113,49 @@ namespace H1ddenGames
                 }
             }
 
-            public void OnPointerClick(PointerEventData eventData)
-            {
-                // Make rigidbodies only react to physics.
-                ToggleRigidbodies(false);
+            public void OnPointerDown(PointerEventData eventData) {
+
+                if(eventData.clickCount == 0)
+                {
+                    HitsToBreak -= 1;
+                } else
+                {
+                    HitsToBreak -= eventData.clickCount;
+                }
 
                 // Get current position of each fractured part.
                 UpdateDistances();
 
-                int i = 0;
-                foreach (var item in Rigidbodies)
+                if (HitsToBreak > 0)
                 {
-                    // If the distance from the clickable object is greater than the effect's distance, there's nothing to do. 
-                    if (Distances[i] > EffectDistance) { i++; }
-                    else {
-                        i++;
+                    _animator.SetTrigger("isHit");
+                    return;
+                }
+                else if (HitsToBreak == 0)
+                {
+                    // Make rigidbodies only react to physics.
+                    ToggleRigidbodies(false);
+
+                    if (IsExplosive == true)
+                    {
+                        foreach (var item in Rigidbodies)
+                        {
+                            // Otherwise, get the rigidbody and add an explosion force.
+                            Vector3 location = new Vector3(
+                                transform.position.x + Random.Range(MinExplosionRange, MaxExplosionRange),
+                                transform.position.y + Random.Range(MinExplosionRange, MaxExplosionRange),
+                                transform.position.z + Random.Range(MinExplosionRange, MaxExplosionRange)
+                            );
+
+                            item.AddExplosionForce(
+                                Random.Range(MinExplosionForce, MaxExplosionForce),
+                                location,
+                                Random.Range(MinExplosionRadius, MaxExplosionRadius)
+                            );
+                        }
+                    }
+                    else
+                    {
                         // Otherwise, get the rigidbody and add an explosion force.
                         Vector3 location = new Vector3(
                             transform.position.x + Random.Range(MinExplosionRange, MaxExplosionRange),
@@ -127,15 +163,22 @@ namespace H1ddenGames
                             transform.position.z + Random.Range(MinExplosionRange, MaxExplosionRange)
                         );
 
-                        item.AddExplosionForce(
+                        Rigidbodies[Random.Range(0, Rigidbodies.Count - 1)].AddExplosionForce(
                             Random.Range(MinExplosionForce, MaxExplosionForce),
                             location,
                             Random.Range(MinExplosionRadius, MaxExplosionRadius)
                         );
                     }
+
+                    UpdateClickableArea();
                 }
 
                 co = StartCoroutine("CleanUp");
+            }
+
+            public void OnPointerUp(PointerEventData eventData)
+            {
+                
             }
 
             public IEnumerator CleanUp()
@@ -173,59 +216,11 @@ namespace H1ddenGames
                 }
             }
 
-            /// <summary>
-            /// Updates the box collider's shape based on a raycast to see if there's any material to manipulate.
-            /// </summary>
             public void UpdateClickableArea()
             {
-                UpdateArea("top");
-                UpdateArea("center");
-                UpdateArea("bottom");
-            }
-
-            public void UpdateArea(string location)
-            {
-                // Change the material of the object pieces that are outside of the effect range.
-                ReplaceMaterials();
-
-                // Once the collider gets below a certain size, there is no need to have it active anymore.
-                if (BoxCollider.size.x < 0.5f || BoxCollider.size.y < 0.5f || BoxCollider.size.z < 0.5f) {
-                    if(BoxCollider.enabled == false)
-                    {
-                        return;
-                    }
-
+                if(HitsToBreak <= 0) {
                     BoxCollider.enabled = false;
-                    return;
-                }
-
-                // The "~" negates the statement after it. So instead of ignoring "Destructible" layer objects, it will ignore everything BUT the "Destructible" layer.
-                LayerMask _layerMask = ~LayerMask.NameToLayer("Destructible");
-                Vector3 size = new Vector3(BoxCollider.size.x, BoxCollider.size.y / 3, BoxCollider.size.z);
-                List<Collider> hitColliders = new List<Collider>();
-                hitColliders.Clear();
-
-                if (location.ToLower().Equals("top"))
-                {
-                    // Top
-                    hitColliders = Physics.OverlapBox(BoxCollider.center * (BoxCollider.size.y / 2.5f), size, Quaternion.identity, _layerMask).ToList();
-                }
-                else if(location.ToLower().Equals("center"))
-                {
-                    // Center
-                    hitColliders = Physics.OverlapBox(BoxCollider.center, size, Quaternion.identity, _layerMask).ToList();
-                } else if(location.ToLower().Equals("bottom"))
-                {
-                    // Bottom
-                    hitColliders = Physics.OverlapBox(BoxCollider.center / BoxCollider.size.y, size, Quaternion.identity, _layerMask).ToList();
-                }
-
-                // Decrease the size of the collider if there is nothing found within the collider.
-                if(hitColliders.Count <= Rigidbodies.Count / 2)
-                {
-                    BoxCollider.size = new Vector3(BoxCollider.size.x, BoxCollider.size.y / 3, BoxCollider.size.z);
-                    BoxCollider.center = new Vector3(BoxCollider.center.x, BoxCollider.size.y / 3, BoxCollider.center.z);
-                }
+                } else { return; }
             }
         }
     }
