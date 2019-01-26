e06220da59d1f201d073aa01401cfa65f5f67c24
H1ddenGames <skarim3000@gmail.com>
2019-01-25 21:23:34 -0800

    Added a simple crafting system and a humanoid to test animations

---
+++ Assets/Scripts/Other Scripts/Crafting/CraftSO.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace H1ddenGames
+{
+    namespace ItemSystems
+    {
+        [CreateAssetMenu(fileName = "New Crafting Recipe", menuName = "H1ddenGames/ItemSystem/Crafting Scriptable Object")]
+        public class CraftSO : ScriptableObject
+        {
+            [SerializeField] private List<ItemSO> listOfRequiredItems = new List<ItemSO>();
+            [SerializeField] private ItemSO resultOfCrafting;
+
+            #region Getters and Setters
+            public List<ItemSO> ListOfRequiredItems { get => listOfRequiredItems; set => listOfRequiredItems = value; }
+            public ItemSO ResultOfCrafting { get => resultOfCrafting; set => resultOfCrafting = value; }
+            #endregion
+        }
+    }
+}
